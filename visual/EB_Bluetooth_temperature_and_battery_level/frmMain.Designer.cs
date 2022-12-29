9ea956ce76f53f26897aadab7678dc408a1c5e2e
DavidKopalaEarable <lctuyenvn@gmail.com>
2022-12-29 14:52:15 +0700

    upload code
    test 1

---
+++ visual/EB_Bluetooth_temperature_and_battery_level/frmMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace EB_Bluetooth_temperature_and_battery_level
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Port_deviceTest = new System.IO.Ports.SerialPort(this.components);
+            this.cbx_select_port = new System.Windows.Forms.ComboBox();
+            this.lbl_select_port = new System.Windows.Forms.Label();
+            this.btn_start_log = new System.Windows.Forms.Button();
+            this.btn_stop_log = new System.Windows.Forms.Button();
+            this.tbx_output = new System.Windows.Forms.TextBox();
+            this.tmr_logging_data = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbx_select_port
+            // 
+            this.cbx_select_port.FormattingEnabled = true;
+            this.cbx_select_port.Location = new System.Drawing.Point(123, 29);
+            this.cbx_select_port.Name = "cbx_select_port";
+            this.cbx_select_port.Size = new System.Drawing.Size(121, 21);
+            this.cbx_select_port.TabIndex = 0;
+            // 
+            // lbl_select_port
+            // 
+            this.lbl_select_port.AutoSize = true;
+            this.lbl_select_port.Location = new System.Drawing.Point(28, 37);
+            this.lbl_select_port.Name = "lbl_select_port";
+            this.lbl_select_port.Size = new System.Drawing.Size(59, 13);
+            this.lbl_select_port.TabIndex = 1;
+            this.lbl_select_port.Text = "Chọn cổng";
+            // 
+            // btn_start_log
+            // 
+            this.btn_start_log.Location = new System.Drawing.Point(31, 88);
+            this.btn_start_log.Name = "btn_start_log";
+            this.btn_start_log.Size = new System.Drawing.Size(213, 36);
+            this.btn_start_log.TabIndex = 2;
+            this.btn_start_log.Text = "Bắt đầu log";
+            this.btn_start_log.UseVisualStyleBackColor = true;
+            this.btn_start_log.Click += new System.EventHandler(this.btn_start_log_Click);
+            // 
+            // btn_stop_log
+            // 
+            this.btn_stop_log.Location = new System.Drawing.Point(31, 142);
+            this.btn_stop_log.Name = "btn_stop_log";
+            this.btn_stop_log.Size = new System.Drawing.Size(213, 36);
+            this.btn_stop_log.TabIndex = 3;
+            this.btn_stop_log.Text = "Kết thúc log";
+            this.btn_stop_log.UseVisualStyleBackColor = true;
+            this.btn_stop_log.Click += new System.EventHandler(this.btn_stop_log_Click);
+            // 
+            // tbx_output
+            // 
+            this.tbx_output.Location = new System.Drawing.Point(302, 20);
+            this.tbx_output.Multiline = true;
+            this.tbx_output.Name = "tbx_output";
+            this.tbx_output.Size = new System.Drawing.Size(462, 392);
+            this.tbx_output.TabIndex = 4;
+            // 
+            // tmr_logging_data
+            // 
+            this.tmr_logging_data.Interval = 2000;
+            this.tmr_logging_data.Tick += new System.EventHandler(this.tmr_logging_data_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbx_output);
+            this.Controls.Add(this.btn_stop_log);
+            this.Controls.Add(this.btn_start_log);
+            this.Controls.Add(this.lbl_select_port);
+            this.Controls.Add(this.cbx_select_port);
+            this.Name = "frmMain";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort Port_deviceTest;
+        private System.Windows.Forms.ComboBox cbx_select_port;
+        private System.Windows.Forms.Label lbl_select_port;
+        private System.Windows.Forms.Button btn_start_log;
+        private System.Windows.Forms.Button btn_stop_log;
+        private System.Windows.Forms.TextBox tbx_output;
+        private System.Windows.Forms.Timer tmr_logging_data;
+    }
+}
+
---
+++ visual/EB_Bluetooth_temperature_and_battery_level/frmMain.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace EB_Bluetooth_temperature_and_battery_level
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cbx_select_port.Items.AddRange(ports);
+            cbx_select_port.SelectedIndex = 0;
+        }
+        public void ATCommand_init()
+        {
+            try
+            {
+                Port_deviceTest.PortName = cbx_select_port.SelectedItem.ToString();
+                Port_deviceTest.BaudRate = 9600;
+                Port_deviceTest.DataBits = 8;
+                Port_deviceTest.Parity = Parity.None;
+                Port_deviceTest.StopBits = StopBits.One;
+                Port_deviceTest.NewLine = "\r\n";
+                Port_deviceTest.Handshake = Handshake.None;
+                Port_deviceTest.DtrEnable = true;
+                Port_deviceTest.WriteBufferSize = 1024;
+                Port_deviceTest.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+                Port_deviceTest.Open();
+
+                if (Port_deviceTest.IsOpen)
+                {
+                    Port_deviceTest.WriteLine("AT + TESTFUNNY");
+                    Console.WriteLine("AT + TESTFUNNY");
+                    Port_deviceTest.WriteLine("AT + TEMPERATURE");
+                    Console.WriteLine("AT + TEMPERATURE");
+                    Port_deviceTest.WriteLine("AT + BATTERYLEVEL");
+                    Console.WriteLine("AT + BATTERYLEVEL");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Port_deviceTest Init False");
+            }
+        }
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            char[] Chars_delimiter = { ' ', ',', '.', ':', '\t', '\n' };
+            string Data_in_str = Port_deviceTest.ReadExisting();
+            string[] Data_in_arr = Data_in_str.Split(Chars_delimiter);
+            if (Data_in_arr[1].IndexOf("OK") >= 0) return;
+            String Data_row_on_time = "";
+            Console.Write(DateTime.Now.ToString("h:mm:ss tt"));
+            Console.Write(',');
+            Console.Write(Data_in_arr[1]);
+            Console.Write(',');
+            Console.Write(Data_in_arr[2]);
+            Data_row_on_time = DateTime.Now.ToString("h:mm:ss tt") + "," + Data_in_arr[1] + "," + Data_in_arr[2];
+            SetText(this.tbx_output.Text.ToString() + '\n' + Data_row_on_time);
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.tbx_output.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.tbx_output.Text = text;
+            }
+        }
+        private void btn_start_log_Click(object sender, EventArgs e)
+        {
+            ATCommand_init();
+            tmr_logging_data.Enabled = true;
+        }
+
+        private void btn_stop_log_Click(object sender, EventArgs e)
+        {
+            tmr_logging_data.Enabled = false;
+
+        }
+
+        private void tmr_logging_data_Tick(object sender, EventArgs e)
+        {
+            Port_deviceTest.WriteLine("AT + TEMPERATURE");
+            Console.WriteLine("AT + TEMPERATURE");
+            Port_deviceTest.WriteLine("AT + BATTERYLEVEL");
+            Console.WriteLine("AT + BATTERYLEVEL");
+        }
+    }
+}
